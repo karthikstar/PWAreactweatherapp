ef848adf15be95f7d8984ce17c33ab5adb90655d
karthikstar <64301417+karthikstar@users.noreply.github.com>
2020-07-10 19:26:00 +0800

    adding build folder

---
+++ build/precache-manifest.384c48118f47d5085c74ad04523990e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389ad9809eede0ca27b7d1abfcf5c813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f893ea9993e47639550",
+    "url": "/static/css/main.b4be5c2b.chunk.css"
+  },
+  {
+    "revision": "50296f539d31895bfe6b",
+    "url": "/static/js/2.c4d0585e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4d0585e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f893ea9993e47639550",
+    "url": "/static/js/main.784faa99.chunk.js"
+  },
+  {
+    "revision": "d11f618a3eed5ef79251",
+    "url": "/static/js/runtime-main.5aefa577.js"
+  }
+]);
